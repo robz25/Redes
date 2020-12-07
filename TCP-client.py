171e72178cef11c235a39060e293fd1b98a0a97b
robz25 <weiserfi@outlook.com>
2020-12-06 18:58:46 -0600

    comentarios letras
    agregaron comentarios, conversion de letras, conversion de string a byte

--- TCP-client.py
+++ TCP-client.py
@@ -1,13 +1,13 @@
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = '127.0.0.1'  # direccion de loopback o localhost
+PORT = 3333        # Puerto empleado en el servidor
 enviar = 'acbdeertykilo'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    enviar = bytes(enviar, 'utf-8') #sin esta linea da error
+    enviar = bytes(enviar, 'utf-8') #convertir string a byte
     s.send(enviar)
     data = s.recv(1024)
 
-print('Received', repr(data))
+print('Received', repr(data))   #mostrar informacion recibida
--- TCP-server.py
+++ TCP-server.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = '127.0.0.1'  # direccion de loopback o localhost
-PORT = 65432        # puerto de escucha, puertos sin privilegios arriba de 1023
+PORT = 3333        # puerto de escucha, puertos sin privilegios arriba de 1023
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -13,7 +13,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-#            data = str(data)
-            data = data.upper()
-#            data = bytes(data, 'utf-8')
+            data = data.upper() #convertimos letras a muysculas
             conn.send(data)
