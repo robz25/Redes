42685dce7eabf8cbe614014df70d8cd0287fd51e
MichG12 <40402571+MichG12@users.noreply.github.com>
2020-12-06 20:19:26 -0600

    Add files via upload

---
+++ client_udp.py
@@ -0,0 +1,15 @@
+#!usr/bin/python
+
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
+
+udp_host = socket.gethostname()		# Host IP
+udp_port = 12345			        # specified port to connect
+
+msg = "Hello Python!"
+msg = bytes(msg,'utf-8') 
+print ("UDP target IP:", udp_host)
+print ("UDP target Port:", udp_port)
+
+sock.sendto(msg,(udp_host,udp_port))		# Sending message to UDP server
